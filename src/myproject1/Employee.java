2ad0c748b987807aeaa743698673472b162f8e54
jagadeesh <gouragallajagadeesh@gmail.com>
2024-06-06 11:11:18 +0530

    EMPLOYEE IS UPDATED

--- src/myproject1/Employee.java
+++ src/myproject1/Employee.java
@@ -4,5 +4,23 @@ public class Employee {
 	private int employeeid;
 	private String employeename;
 	private long employeemobile;
+	public int getEmployeeid() {
+		return employeeid;
+	}
+	public void setEmployeeid(int employeeid) {
+		this.employeeid = employeeid;
+	}
+	public String getEmployeename() {
+		return employeename;
+	}
+	public void setEmployeename(String employeename) {
+		this.employeename = employeename;
+	}
+	public long getEmployeemobile() {
+		return employeemobile;
+	}
+	public void setEmployeemobile(long employeemobile) {
+		this.employeemobile = employeemobile;
+	}
 
 	}
